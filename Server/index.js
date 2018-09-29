fcfaf55b45b1989bfefc6cc6374519c658ba2c0b
Glenn Lundberg <glenn.lundberg@creuna.se>
2018-09-29 11:00:04 +0200

    added babel

---
+++ Server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.get('types', (req,res) => res.send())
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
